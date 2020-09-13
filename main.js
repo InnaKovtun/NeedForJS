8b8488e26feeaad5f86ddbfd56e0e490f51d547c
Inna Kovtun <39884162+InnaKovtun@users.noreply.github.com>
2020-09-13 19:37:12 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,20 +18,77 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
 };
+
+function getQuenityElements(heughtElement){
+    return document.documentElement.clientHeight / heughtElement + 1;
+}
 
 function startGame() {
     start.classList.add('hide');
+    gameArea.innerHTML = ''; //очищение поля
+
+    for(let i = 0; i < getQuenityElements(100); i++){
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
+    for(let i =0; i < getQuenityElements(100 * setting.traffic); i++){
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor((Math.random() * (gameArea.offsetWidth - 50))) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url(./img/enemy2.png) center / cover no-repeat';
+        gameArea.appendChild(enemy);
+
+    }
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    
+    //размещение автомобиля
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
+
+    setting.x = car.offsetLeft; 
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame); //запуск анимированной функции
 }
 
 function playGame() {
-    console.log('Play game');
-    //пока старт = тру функция перезапускается
+    
     if(setting.start){
+        moveRoad();
+        moveEnemy();
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE<br> ' + setting.score;
+
+        if(keys.ArrowLeft && setting.x > 0){
+            setting.x -= setting.speed;
+        }
+
+        if(keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)){
+            setting.x += setting.speed;
+        }
+
+        if(keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)){
+            setting.y += setting.speed;
+        }
+
+        if(keys.ArrowUp && setting.y > 0){
+            setting.y -= setting.speed;
+        }
+
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
+
         requestAnimationFrame(playGame); //рекурсия для плавной правильной работы
     } 
 }
@@ -43,7 +100,47 @@ function startRun(event) {
 
 function stopRun(event) {
     event.preventDefault();
-    keys[event.key] = true;
+    keys[event.key] = false;
+}
+
+function moveRoad() {
+    let lines = document.querySelectorAll('.line');
+    lines.forEach(function(line){
+        line.y += setting.speed;
+        line.style.top = line.y + 'px';
+
+        if(line.y >= document.documentElement.clientHeight){
+            line.y = -100;
+        }
+    });
+}
+
+function moveEnemy() {
+    let enemy = document.querySelectorAll('.enemy');
+
+    enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect(); //параменры авто
+        let enemyRect = item.getBoundingClientRect(); //параметры врагов
+
+        //проверка пересечения машин с помощью параметров
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top){
+                setting.start = false;
+                start.classList.remove('hide');
+                start.style.top = score.offsetHeight;
+        }
+
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+
+        if (item.y >= document.documentElement.clientHeight){
+            item.y = -100 * setting.traffic;
+            item.style.left = Math.floor((Math.random() * (gameArea.offsetWidth - 50))) + 'px';
+        }
+
+    });
 }
 
 
